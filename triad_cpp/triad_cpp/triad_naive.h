b0bac1a85799263e102e0e025b357493822057bc
Oztoprak <ga53zeg@mytum.de>
2019-10-21 17:04:36 +0200

    init
    Former-commit-id: 8665961d606b22db4a9360a438625facb30339ac [formerly cf7740c73c4a3378b0161e1002702bf75cd2776d]
    Former-commit-id: 6281b3c56b31fe3595625d853fe5a7b6c4b50e23
    Former-commit-id: b1accd2c0b4dd7d0ff0942ee48c380bb97f47ded

---
+++ triad_cpp/triad_cpp/triad_naive.h
@@ -0,0 +1,8 @@
+#ifndef __TRIAD_NAIVE_H
+#define __TRIAD_NAIVE_H
+#include <vector>
+
+double triad_naive(double* A, const double* B, const double* C, const double* D, int size, int iter);
+
+#endif
+
