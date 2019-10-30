1810403bed3f9a86f2df0d77bf1ef5f4c07d6abf
sariug <ugurcansari93@gmail.com>
2019-10-30 17:36:58 +0100

    to next
    Former-commit-id: d0d7e18d8ae8f0fce9310c6ae36c2341e5ca11b6

--- triad_cpp/triad_cpp/triad_naive.h
+++
@@ -1,8 +0,0 @@
-#ifndef __TRIAD_NAIVE_H
-#define __TRIAD_NAIVE_H
-#include <vector>
-
-double triad_naive(double* A, const double* B, const double* C, const double* D, int size, int iter);
-
-#endif
-
