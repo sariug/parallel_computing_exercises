5828e849e45fd801b924d209a67661b7fee53cec
sariug <ugurcansari93@gmail.com>
2019-10-30 17:39:25 +0100

    triad init
    Former-commit-id: ec55025b650520a912dcdd42a4871c9038cb0e15
    Former-commit-id: ce5bf48817d7a4bc091771e5eb9c98fca1b446af
    Former-commit-id: 16fef545a29b8cf476a3f80cc50396bae33dc1a5

---
+++ triad_cpp/triad_cpp/triad_algo.h
@@ -0,0 +1,7 @@
+#ifndef __TRIAD_NAIVE_H
+#define __TRIAD_NAIVE_H
+#include <vector>
+
+double triad_algo(std::vector<double>& A, const std::vector<double>& B, const std::vector<double>& C, const std::vector<double>& D, int iter);
+
+#endif
