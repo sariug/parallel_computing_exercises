a08633da4bf2e51d1c5ada7ddbb7dfe82418f4bd
sariug <ugurcansari93@gmail.com>
2019-11-08 11:46:58 +0100

    thread asnwer

--- 06_11_2019/benchmark_answer/answer.cpp
+++ 06_11_2019/benchmark_answer/answer.cpp
@@ -8,19 +8,14 @@
 #include <string>
 #include <thread>
 #include <numeric>
+/*
+	An example of using threads to calculate the so-called norm(ish) function.
 
+	Author: Uğurcan Sarı, Oguz Oztoprak
+	Date : 06.11.2019
 
-// Fill
-double norm(double* A, double* B, size_t size)
-{
-    double sum = 0.0;
-    for (size_t i = 0; i < size; ++i )
-    {
-        sum += A[i] * B[i];
-    }
+*/
 
-    return std::sqrt(sum); 
-}
 
 void normThread(double* A, double* B,  int size, int start, int end, double& sum)
 {
@@ -34,15 +29,11 @@ void normThread(double* A, double* B,  int size, int start, int end, double& sum
 
 }
 
-void randomizedTest( size_t );
-void randomizedTest2( size_t );
 int main()
 {
     int size;
     std::cout<<"Please enter the length of the array: ";
     std::cin >> size;
-
-    //randomizedTest(static_cast<size_t>(size));
       
     int numberOfThreads = 6;
 
@@ -52,7 +43,7 @@ int main()
     std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
     std::uniform_real_distribution<> dis(1.0, 1000.0);
 
-    
+    // Create and fill the arrays
     double* A = new double[size];
     double* B = new double[size];
     for (int n = 0; n < size; ++n) 
@@ -60,6 +51,7 @@ int main()
         A[n] = dis(gen);
         B[n] = dis(gen);
     }
+    // start timer
     auto start = std::chrono::high_resolution_clock::now();
     std::vector<double> sums(numberOfThreads, 0.0);
 
@@ -89,59 +81,4 @@ int main()
     duration = end - start;
     std::cout <<"\nReference test took " << duration.count() << " ms\n";
     return 0;
-
-}
-
-void randomizedTest( size_t my_size)
-{
-    std::random_device rd;  //Will be used to obtain a seed for the random number engine
-    std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
-    std::uniform_real_distribution<> dis(1.0, 1000.0);
-
-    
-    double* A = new double[my_size];
-    double* B = new double[my_size];
-    for (size_t n = 0; n < my_size; ++n) 
-    {
-        A[n] = dis(gen);
-        B[n] = dis(gen);
-    }
-
-    auto start = std::chrono::high_resolution_clock::now();
-    double normAns = norm(A,B,my_size);
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double,std::milli> duration = end - start;
-    double mflopSeconds = (2*my_size)* 1e-6 / (duration.count() * 1e-3) ;
-    std::cout <<"\nCalculated value with my function : " << normAns << '\n';
-    std::cout <<"Test took " << duration.count() << " ms\n";
-    std::cout <<"MFLOP/s " << mflopSeconds<<'\n';
-
-    start = std::chrono::high_resolution_clock::now();
-    normAns =  std::sqrt( std::inner_product(A,A+my_size, B,0.0)); 
-    end = std::chrono::high_resolution_clock::now();
-    duration = end - start;
-    mflopSeconds = (2*my_size)* 1e-6 / (duration.count() * 1e-3) ;
-    std::cout <<"\nReference value : "<< normAns << '\n';
-    std::cout <<"Test took " << duration.count() << " ms\n";
-    std::cout <<"MFLOP/s " << mflopSeconds<<'\n';
-
-}
-void randomizedTest2( size_t my_size)
-{
-    double* A = new double[my_size];
-    double* B = new double[my_size];
-    for (size_t n = 0; n < my_size; ++n) 
-    {
-        A[n] = 2.;
-        B[n] = 3.;
-    }
-
-    auto start = std::chrono::high_resolution_clock::now();
-    double normAns = norm(A,B,my_size);
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double,std::milli> duration = end - start;
-    std::cout <<"Test took " << duration.count() << " ms\n";
-    std::cout <<"answer: " << normAns << '\n';
-    assert( normAns == std::sqrt( std::inner_product(A, A + my_size, B, 0.0)) );
-
 }
