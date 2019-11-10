7d449fabf4513338a701e57e0c19946fdcbe203b
oguz <oguzoztoprak@gmail.com>
2019-11-10 22:37:59 +0100

    1d linear convection template added

---
+++ 13_11_2019/1D_convection/1D_linear_convection.cpp
@@ -0,0 +1,43 @@
+/*
+	A simple 1-D Linear Convection, originates from Lorena A. Barba, Gilbert F. Forsyth 
+
+    https://nbviewer.jupyter.org/github/barbagroup/CFDPython/blob/master/lessons/01_Step_1.ipynb
+
+	Author: Uğurcan Sarı, Oguz Oztoprak
+	Date : 13.11.2019
+
+*/
+int main()
+{
+    /* Define parameters ( uncomment )
+    
+    int nx = 41; // Has to be odd number
+    double dx = 2. / (nx - 1 );
+    int nt = 25;
+
+    double dt = 0.025;
+
+    double c = 1.;
+    */
+
+    // Create a vector u of length nx, will it with 1
+    
+    
+    
+    // Apply initial conditions, that is replace some of the 1's with 2's. Look at the explanation.
+    
+
+
+    // Loop over nt time steps, use the numerical scheme.
+    
+    
+    // Print out the u vector at each time step, compare your results with the original python implementation.
+    
+    
+    // Next: write each time step to a file and visualize using your favorite scripting language 
+    // Next Next : Can you use threads to speed up any for loops? Implement for nx = 1001
+    
+
+
+    return 0;
+}
