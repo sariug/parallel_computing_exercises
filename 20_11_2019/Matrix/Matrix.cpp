0d9b61ec0d7f2fe73d865a2811d9ba60dd543da1
Oguz O <38984696+JusticeBoi@users.noreply.github.com>
2019-11-20 19:36:32 +0100

    Update Matrix.cpp

--- 20_11_2019/Matrix/Matrix.cpp
+++ 20_11_2019/Matrix/Matrix.cpp
@@ -16,16 +16,21 @@ Matrix::Matrix(int nRows, int nCols )
     m_matrix = std::vector<std::vector<double>>(nRows, std::vector<double>(nCols, 0.0));
 }
 
-size_t Matrix::numberOfCols()
+size_t Matrix::numberOfCols() const
 {
     return m_matrix[0].size();
 }
-size_t Matrix::numberOfRows()
+size_t Matrix::numberOfRows() const
 {
     return m_matrix.size();
 }
 
 double& Matrix::operator()(int x, int y)
+{
+    return m_matrix[x][y];
+}
+
+const double& Matrix::operator()(int x, int y) const
 {
     return m_matrix[x][y];
 }
