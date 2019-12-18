d9a4282a299c270b5dcf62a9038499e8a709c3eb
sariug <ugurcansari93@gmail.com>
2019-12-18 18:31:17 +0100

    Update count1000.cpp

--- 18_12_2019/count1000/count1000.cpp
+++ 18_12_2019/count1000/count1000.cpp
@@ -1,28 +1,7 @@
 #include <iostream>
-#include <thread>
-#include <vector>
-#include <chrono>
-#include <mutex>
-int my_count = 0;
-std::mutex my_count_mutex;
-
-void count()
-{
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    my_count_mutex.lock();
-    my_count++;
-    my_count_mutex.unlock();
-}
 
 int main()
 {
 
-    std::vector<std::thread> threads;
-
-    for (int i = 0; i < 1000; ++i)
-        threads.emplace_back(count);
-	for (auto &thread : threads)
-		thread.join();
-    std::cout << "We counted until " << my_count << "." << std::endl;
     return 0;
 }
