a00eec42a2701dabfedd04fe246222ff273c40d2
justiceboi <oguzoztoprak@gmail.com>
2020-01-22 18:44:57 +0100

    define trials

--- 22_01_2020/exercises/main.cpp
+++ 22_01_2020/exercises/main.cpp
@@ -5,11 +5,14 @@ void test1( );
 void test2( );
 int main()
 {
-
+#ifdef PAR_COMP_OMP_ENABLED
 #pragma omp parallel
     {
         std::cout << omp_get_thread_num() << '\n'; 
     }
+#else
+std::cout << "hello world" << '\n'; 
+#endif
 	return 0;
 }
 
